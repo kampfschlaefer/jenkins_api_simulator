3886cb0bddad1055be488c5f56469f1d55055e3b
Arnold Krille <arnold@arnoldarts.de>
2015-03-30 22:58:07 +0200

    second type of requests passed through

--- jenkins_api_simulator/passthrough.py
+++ jenkins_api_simulator/passthrough.py
@@ -11,8 +11,16 @@ app.debug = True
 jenkins_url = 'http://localhost:8000'
 
 
+def prepare_response(response):
+    _headers = {}
+    for h in ['content-type', 'x-jenkins-session', 'x-jenkins', 'server']:
+        _headers[h] = response.headers[h]
+    _content = response.text.replace(jenkins_url, request.url_root.strip('/'))
+    return make_response((_content, response.status_code, _headers))
+
+
 @app.route('/api/python', methods=['GET'])
-def handle_forward():
+def handle_api_python():
     logger.info('Request for /api/python')
     logger.info(
         'path %s, method %s, data "%s"',
@@ -20,16 +28,25 @@ def handle_forward():
     )
     if request.method == 'GET':
         r = requests.get('%s%s' % (jenkins_url, request.path))
-        logger.info(
-            'got reponse from jenkins:\n  status_code = %s\n'
-            '  headers =\n%s\n  text =\n%s' % (
-                r.status_code, r.headers, r.text
-            )
-        )
-        headers = {}
-        for h in ['content-type', 'x-jenkins-session', 'x-jenkins', 'server']:
-            headers[h] = r.headers[h]
-        response = make_response((r.text, r.status_code, headers))
-        return response
+        return prepare_response(r)
+        # content = r.text.replace(jenkins_url, request.url_root.strip('/'))
+        # logger.info(
+        #     'got reponse from jenkins:\n  status_code = %s\n'
+        #     '  headers =\n%s\n  text =\n%s' % (
+        #         r.status_code, r.headers, content
+        #     )
+        # )
+        # headers = {}
+        # for h in ['content-type', 'x-jenkins-session', 'x-jenkins', 'server']:
+        #     headers[h] = r.headers[h]
+        # response = make_response((content, r.status_code, headers))
+        # return response
 
     return '{}'
+
+
+@app.route('/job/<name>/api/python', methods=['GET'])
+def handle_job(name):
+    logger.info('Requesting job %s', name)
+    r = requests.get('%s%s' % (jenkins_url, request.path))
+    return prepare_response(r)
--- tests/test_passthrough.py
+++ tests/test_passthrough.py
@@ -5,6 +5,7 @@ import pytest
 from pytest_localserver.http import WSGIServer
 
 from jenkins_api_simulator import passthrough
+passthrough.jenkins_url = 'http://localhost:8080'
 
 
 @pytest.yield_fixture(scope='module')
@@ -15,9 +16,20 @@ def passthrough_server():
     _server.stop()
 
 
-def test_get_jobs(passthrough_server):
-    passthrough.jenkins_url = 'http://localhost:8080'
+@pytest.fixture
+def myjenkins(passthrough_server):
+    return jenkins.Jenkins(passthrough_server.url)
 
-    j = jenkins.Jenkins(passthrough_server.url)
-    logging.info(j.keys())
-    assert j.keys()
+
+def test_get_jobs(myjenkins):
+    logging.info(myjenkins.keys())
+    assert myjenkins.keys()
+
+
+def test_get_first_job(myjenkins):
+    jobname = myjenkins.keys()[0]
+    assert jobname
+
+    first_job = myjenkins[jobname]
+    assert first_job is not None
+    assert first_job.name == jobname
